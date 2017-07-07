a9eccf595cda412caf8b6feb0569aabfa4b1683d
Taylor Sorenson <taylorsorenson@Taylors-MacBook-Pro-2.local>
2017-07-07 11:09:15 -0400

    adding functions that may be useful in the SIM1 class

--- __main__.py
+++ __main__.py
@@ -1,5 +1,7 @@
 import sys
 import logging
+import os
+import numpy as np
 
 from FileProcessingService import FileProcessingService
 from ThirdPartyProgramCaller import ThirdPartyProgramCaller
@@ -38,6 +40,32 @@ def callThirdPartyService(file_extension, path):
 def processInputFileAndCreateGenomePermutations(data_file, file_extension, path, permutations):
     file_parsing_service = FileProcessingService(data_file, file_extension, permutations, path)
     return file_parsing_service.createGenomePermutations()
+
+#Begin SIM1 using Octave: --> #TODO - Maybe incorporate these into SIM1
+def createRTrialFilesAndGenomeCallFile(R,K): #FileProcessingServiceSim1
+    f = open('sim1/run_simulation_readGenome.m.u')
+    process_R_Files = FileProcessingService(f,'m',K,R,os.getcwd())
+    return process_R_Files.handleRFilesForSIM1UsingMATLABOrOctave()
+
+def runRSimulationsAndCreateOutputList(): #
+    octaveCall = ThirdPartyProgramCaller(os.getcwd(),'MAT')
+    outputList = octaveCall.callOctave('/GenomeFiles','/GenomeCallFile')
+    return outputList
+
+def replaceGenomeFileNamefromFiles(trialFileList):
+    for trialFile in trialFileList:
+        pass #TODO - Replace every appearance of a file name with an input filename
+
+def RunAllKGenomesAndCreateMatrix(R,K):
+    outputMatrix = []
+    trialFileList = createAndNameRTrialFiles(R,K)
+    for genome in range(K):
+        outputRow = runRSimulationsAndCreateOutputList()
+        replaceGenomeFileNamefromFiles(trialFileList) #TODO - pass in a list? Or smthg like a directory?
+        outputMatrix.append(outputRow)
+    return outputMatrix
+
+
 
 
 if __name__ == "__main__":
