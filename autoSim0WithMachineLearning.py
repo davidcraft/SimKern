d68bf53525f03e248732d083f1a1b2024204bf34
dskrane <dsk2@williams.edu>
2017-07-14 13:54:04 -0400

    Automates sim0 and svm together

---
+++ autoSim0WithMachineLearning.py
@@ -0,0 +1,11 @@
+from FileProcessingService import FileProcessingService
+from ThirdPartyProgramCaller import ThirdPartyProgramCaller
+import svmRunner
+
+def runSim0WithSVM(file, file_type, num_genomes, path, kernel_type, pct_train):
+    file_processor = FileProcessingService(file, file_type, num_genomes, path)
+    [genome_file_list, genomes_matrix] = file_processor.createGenomes()
+    program_caller = ThirdPartyProgramCaller(path, file_type, genome_file_list)
+    responses = program_caller.callThirdPartyProgram()
+    svmRunner.runSvm(responses, genomes_matrix, kernel_type, pct_train)
+
