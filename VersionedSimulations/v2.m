1816808eb409e2da65e2756420592b33fbfad983
aspatti1257 <aspatti1257@gmail.com>
2017-08-17 21:48:38 -0400

    Taylor and Zhaoqi's working v3 replacing my semi-functional v2 version.

--- VersionedSimulations/v2.m
+++ VersionedSimulations/v2.m
@@ -1,62 +1,58 @@
 function v2()
+% set plt = true to plot the graph
+plt = true;
 	%this is the master sim0 version 1.0
 
 	%note for octave compatibility, must install odepkg for octave and also execute the following line
-	%every session.
+	%every session. 
 	%  pkg load odepkg
 	%Alternately, make a file called .octaverc in your home directory, and put that line in the file.
 	%we'll have to verify that this works when calling octave through python.
 
-
+	
 	%ENTITIES in the model:
 	%These are the proteins, mRNA, etc., each of which has an ODE describing its dynamics, which we store in a file
 	%so we can re-use in the ODE function.
 	variableDefinition2
 
-
+	
 	%Initial conditions
 	x0 = zeros(numEntities,1);
 	%here any non-zero initial conditions
-	%for now, we we are modeling our radiation blast as an exponentially decaying level, we just initialize
+	%for now, we we are modeling our radiation blast as an exponetially decaying level, we just initialize
 	%the radiation compartment.
 	x0(O_RADIATION) = 1;
 
 
-	%Simulation time span. We will take the units of time to be MINUTES since that is what Elias paper uses.
+	%Simulation time span. We will take the units of time to be MINUTES since that is what Elias paper uses. 
 
-	Tend_minutes = 24*60; %one day
+        numDays=1;
+	Tend_minutes = 24*60*numDays; %minutes 
 	tspan=[0,Tend_minutes];
 
 	%Just using these default values. They seem fine for now, we might find it useful to adjust later. I'm also using the
 	%low order solver ode23. We may need to change this later too.
-	opts = odeset('AbsTol', 1e-3, 'RelTol', 1e-5, 'MaxStep', 6, 'InitialStep', .1);
-	[t,x]=ode23(@f, tspan, x0, opts);
+	opts = odeset('AbsTol',1e-3,'RelTol',1e-5,'MaxStep',6,'InitialStep',.1);
+	[t,x]=ode23(@f,tspan,x0,opts);
+    if plt == true
+        subplot(1,2,1)
+        varsToPlot = [2 5 6];
+        plot(t,x(:,varsToPlot));
+        legend(N(varsToPlot));
 
-	subplot(3,1,1)
-	varsToPlot = [2 5 6];
-	plot(t,x(:,varsToPlot));
-	legend(N(varsToPlot));
-
-	%here replicate stuff plotted in Elias figure 4.8
-	subplot(3,1,2)
-	varsToPlot = [P_ATMNucPhos P_P53NucPhos P_MDM2Nuc P_WIP1Nuc];
-	h=plot(t,x(:,varsToPlot));
-	set(h(1),'color', 'r');
-	set(h(2),'color', 'g');
-	set(h(3),'color', 'b');
-	set(h(4),'color', 'k');
-	legend(N(varsToPlot));
-
-	%here plot p53-Mdm2 complex from Huzinker et al.
-	subplot(3,1,3)
-	varsToPlot = [P_P53_MDM2_Comp P_P53NucNeg M_MDM2NucNeg P_MDM2NucNeg];
-	h=plot(t,x(:,varsToPlot));
-	set(h(1),'color', 'r');
-	set(h(1),'color', 'g');
-	set(h(1),'color', 'b');
-	set(h(1),'color', 'k');
-	legend(N(varsToPlot));
-
+        %here replicate stuff plotted in Elias figure 4.8
+        subplot(1,2,2)
+%       varsToPlot = [P_ATMNucPhos P_P53NucPhos P_MDM2Nuc P_WIP1Nuc];
+        varsToPlot = [P_ATMNucPhos P_P53NucPhos P_MDM2Nuc P_WIP1Nuc ...
+            O_ARRESTSIGNAL P_ECDK2 P_p21cip];
+        h=plot(t,x(:,varsToPlot));
+        set(h(1),'color', 'r');
+        set(h(2),'color', 'g');
+        set(h(3),'color', 'b');
+        set(h(4),'color', 'k');
+        legend(N(varsToPlot));
+    end
+	
 	function xd=f(t,x)
 
 		%In this function we have the differential equations.
@@ -65,7 +61,7 @@ function v2()
 		%down too much. I don't think it will. If it does we might consider using globals or something like that.
 
 		variableDefinition2
-
+		
 		%We start with all the parameters of those equations. We might want to think a little more
 		%about an organized nomenclature for these. For example it would be nice to be able to tell from the name
 		%basically what it is doing. But we also don't want to be cumbersome and have really long names (I think..).
@@ -87,38 +83,38 @@ function v2()
 		%do it differently]
 		% the system's constants, for the full description see Table B.1
 
-
-		k3=3;
+		
+		k3=3; 
 		Katm=0.1;
-		kdph1=78;
+		kdph1=78; 
 		Kdph1=25;
-		k1=10;
+		k1=10; 
 		K1=1.01;
-		pp=0.083;
+		pp=0.083; 
 		Vr=10;
-		pm=0.04;
-		deltam=0.16;
-		kSm=0.005;
-		kSpm=1;
-		KSpm=0.1;
+		pm=0.04; 
+		deltam=0.16; 
+		kSm=0.005; 
+		kSpm=1; 
+		KSpm=0.1; 
 		pmrna=0.083;
-		deltamrna=0.0001;
+		deltamrna=0.0001; 
 		ktm=1;
-		kS=0.015;
+		kS=0.015; 
 		deltap=0.2;
-		pw=0.083;
-		deltaw=0.2;
-		kSw=0.03;
-		kSpw=1;
-		KSpw=0.1;
+		pw=0.083; 
+		deltaw=0.2; 
+		kSw=0.03; 
+		kSpw=1; 
+		KSpw=0.1; 
 		pwrna=0.083;
-		deltawrna=0.001;
+		deltawrna=0.001; 
 		ktw=1;
-		kph2=15;
 		% this next one to modify the radiation impact:
-		Kph2=.01;
-
-		kdph2=96;
+		kph2=15;
+		Kph2=1;
+		
+		kdph2=96; 
 		Kdph2=26;
 		% nondimensionalisation of the variables is done so that the term relative to
 		% the main bifurcation parameter E depends on as small possible number of
@@ -142,22 +138,43 @@ function v2()
 		barkS=ts*kS/alpha1; bardeltap=ts*deltap;
 		barktm=ts*ktm; barktw=ts*ktw;
 		ATMtot=ATMtot/alphav1;
-
-
-		%Next come the constants from Hunziker et al (https://www.ncbi.nlm.nih.gov/pubmed/20624280):
-		sigma_p = 1000; %p53 production
-		alpha_mdm2 = 0.1; %Mdm2-independent degradation/deactivation of p53
-		delta_mdm2 = 11; %Mdm2-dependent degradation/deactivation of p53
-		k_t_mdm2 = 0.03; %Mdm2 transcription
-		k_tl_mdm2 = 1.4; %Mdm2 translation
-		beta_mdm2 = 0.6; %Mdm2 mRNA degradation
-		gammma_mdm2_deg = 0.2; %degradation/deactivation of Mdm2
-		k_b_pm = 7200; %p53-Mdm2 dissociation
-		k_D_pm = 1.44; %p53-Mdm2-dissociation constant
-		k_f_pm = 5000; %k_b_pm / k_D_pm = p53-Mdm2-complex formation rate constant
-
+        
+        %Apoptosis Rate Constants --> p53 to Cyt c model
+        c_KpB1 = 1;
+        c_KpB2 = 1;
+        c_KpB3 = 1;
+        c_KpBX1 = 1;
+        c_KpBX2 = 1;
+        c_KpBX3 = 1;
+        c_KpF1 = 1;
+        c_KpF2 = 1;
+        c_KpF3 = 1;
+        c_KpBa1 = 1;
+        c_KpBa2 = 1;
+        c_KpBa3 = 1;
+        c_KBaxC = 1;
+        c_KBcl2C = 1;
+        c_KBclXC = 1;
+        c_KCyt = 1;
+        c_KAA = 1;
+        c_KAA2 = 1;
+        c_KApop = 1;
+        c_KApop2 = 1;
+        c_KApop3 = 1;
+        c_Kpp1 = 1;
+        c_Kpp2 = 1;
+        c_Kpp3 = 1;
+        c_KpE1 = 1;
+        c_KpE2 = 1;
+        c_KpE3 = 1;
+        c_KE = 1;
+        KRb = 1; 
+        c_Ka1 = 1;
+        c_Ka2 = 1;
+        Kg = 1;
+		
 		xd = zeros(numEntities,1);
-
+		
 		% the odes
 
 		%DNA impact and repair kinetics
@@ -172,26 +189,26 @@ function v2()
 		%Elias paper https://hal.inria.fr/hal-00822308/document
 		% equations for the nucleus (B.1)
 		% p53
-		xd(P_P53Nuc)=barkdph1*x(P_ATMNucPhos)*(x(P_P53NucPhos)/(barKdph1+x(P_P53NucPhos)))-bark1*x(P_MDM2Nuc)*(x(P_P53Nuc)/(barK1+x(P_P53Nuc))) ...
-					-bark3*x(M_WIP1Nuc)*(x(P_P53Nuc)/(barKatm+x(P_P53Nuc)))-barpp*Vr*(x(P_P53Nuc)-x(P_P53Cyto));
+		xd(P_P53Nuc)=barkdph1*x(P_WIP1Nuc)*(x(P_P53NucPhos)/(barKdph1+x(P_P53NucPhos)))-bark1*x(P_MDM2Nuc)*(x(P_P53Nuc)/(barK1+x(P_P53Nuc))) ...
+					-bark3*x(P_ATMNucPhos)*(x(P_P53Nuc)/(barKatm+x(P_P53Nuc)))-barpp*Vr*(x(P_P53Nuc)-x(P_P53Cyto));
 		% Mdm2
 		xd(P_MDM2Nuc)=-barpm*Vr*(x(P_MDM2Nuc)-x(P_MDM2Cyto))-bardeltam*x(P_MDM2Nuc);
 		% Mdm2 mRNA
 		xd(M_MDM2Nuc)=barkSm+barkSpm*(x(P_P53NucPhos)^4/(barKSpm^4+x(P_P53NucPhos)^4))-barpmrna*Vr*x(M_MDM2Nuc) ...
 					-bardeltamrna*x(M_MDM2Nuc);
 		% p53_p
-		xd(P_P53NucPhos)=bark3*x(M_WIP1Nuc)*(x(P_P53Nuc)/(barKatm+x(P_P53Nuc)))-barkdph1*x(P_ATMNucPhos)*(x(P_P53NucPhos)/(barKdph1+x(P_P53NucPhos)));
+		xd(P_P53NucPhos)=bark3*x(P_ATMNucPhos)*(x(P_P53Nuc)/(barKatm+x(P_P53Nuc)))-barkdph1*x(P_WIP1Nuc)*(x(P_P53NucPhos)/(barKdph1+x(P_P53NucPhos)));
 		%Wip1
-		xd(P_ATMNucPhos)=barpw*Vr*x(P_WIP1Cyto)-bardeltaw*x(P_ATMNucPhos);
+		xd(P_WIP1Nuc)=barpw*Vr*x(P_WIP1Cyto)-bardeltaw*x(P_WIP1Nuc); 
 		%Wip1 mRNA
-		xd(P_WIP1Nuc)=barkSw+barkSpw*(x(P_P53NucPhos)^4/(barKSpw^4+x(P_P53NucPhos)^4))-barpwrna*Vr*x(P_WIP1Nuc) ...
-					-bardeltawrna*x(P_WIP1Nuc);
+		xd(M_WIP1Nuc)=barkSw+barkSpw*(x(P_P53NucPhos)^4/(barKSpw^4+x(P_P53NucPhos)^4))-barpwrna*Vr*x(M_WIP1Nuc) ...
+					-bardeltawrna*x(M_WIP1Nuc);
 		% Atm_p
 		%here we are replacing their E with "broken ends": assuming
 		%"danger signal" is proportional to number of broken ends:
-		%xd(M_WIP1Nuc)=2*barE*( ((ATMtot-x(M_WIP1Nuc))/2)/(1+((ATMtot-x(M_WIP1Nuc))/2) )) -2*barkdph2*x(P_ATMNucPhos)*(x(M_WIP1Nuc)^2/(barKdph2+x(M_WIP1Nuc)^2));
-		xd(M_WIP1Nuc)=2*Kph2*x(O_BROKEN_ENDS)*( ((ATMtot-x(M_WIP1Nuc))/2)/(1+((ATMtot-x(M_WIP1Nuc))/2) )) -2*barkdph2*x(P_ATMNucPhos)*(x(M_WIP1Nuc)^2/(barKdph2+x(M_WIP1Nuc)^2));
-
+		%xd(P_ATMNucPhos)=2*Kph2*0.1*( ((ATMtot-x(P_ATMNucPhos))/2)/(1+((ATMtot-x(P_ATMNucPhos))/2) )) -2*barkdph2*x(P_WIP1Nuc)*(x(P_ATMNucPhos)^2/(barKdph2+x(P_ATMNucPhos)^2));
+		xd(P_ATMNucPhos)=2*Kph2*x(O_BROKEN_ENDS)*( ((ATMtot-x(P_ATMNucPhos))/2)/(1+((ATMtot-x(P_ATMNucPhos))/2) )) -2*barkdph2*x(P_WIP1Nuc)*(x(P_ATMNucPhos)^2/(barKdph2+x(P_ATMNucPhos)^2));
+		
 		% equations for the cytoplasm (B.2)
 		% p53
 		xd(P_P53Cyto)=barkS-bark1*x(P_MDM2Cyto)*(x(P_P53Cyto)/(barK1+x(P_P53Cyto)))-bardeltap*x(P_P53Cyto)-barpp*(x(P_P53Cyto)-x(P_P53Nuc));
@@ -202,14 +219,42 @@ function v2()
 		% Wip1
 		xd(P_WIP1Cyto)=barktw*x(M_WIP1Cyto)-barpw*x(P_WIP1Cyto)-bardeltaw*x(P_WIP1Cyto);
 		% Wip1 mRNA
-		xd(M_WIP1Cyto)=barpwrna*x(P_WIP1Nuc)-barktw*x(M_WIP1Cyto)-bardeltawrna*x(M_WIP1Cyto);
+		xd(M_WIP1Cyto)=barpwrna*x(M_WIP1Nuc)-barktw*x(M_WIP1Cyto)-bardeltawrna*x(M_WIP1Cyto);
+        
+        %Apoptosis --> p53 to Cytochrome c
+        %and apoptosome
+        %BCl-2
+        xd(P_Bcl2) = c_KpB1*(x(P_P53NucPhos)^4)/(c_KpB2 + x(P_P53NucPhos)^4) - c_KpB3 * x(P_Bcl2);
+        %Bcl-Xl
+        xd(P_BclXl) = c_KpBX1*(x(P_P53NucPhos)^4)/(c_KpBX2 + x(P_P53NucPhos)^4) - c_KpBX3 * x(P_BclXl);
+        %Fas-l
+        xd(P_FasL) = c_KpF1*(x(P_P53NucPhos)^4)/(c_KpF2 + x(P_P53NucPhos)^4) - c_KpF3 * x(P_FasL);
+        %BAX
+        xd(P_Bax) = c_KpBa1*(x(P_P53NucPhos)^4)/(c_KpBa2 + x(P_P53NucPhos)^4) - c_KpBa3 * x(P_Bax);
+        %Cytochrome c
+        xd(P_CytC) = c_KBaxC*(x(P_Bax)) - c_KBcl2C*(x(P_Bcl2)) - c_KBclXC*(x(P_BclXl)) - c_KCyt*(x(P_CytC)) ...
+            - c_KAA*x(P_Apoptosome)*x(P_CytC)^7;
+        %Apoptosome
+        xd(P_Apoptosome) = c_KAA*x(P_Apoptosome)*x(P_CytC)^7 - c_KAA2 *x(P_Apoptosome);
+        %Apoptosis
+        xd(P_Apoptosis) = c_KApop*x(P_FasL) + c_KApop2 * x(P_Apoptosome) - c_KApop3 * x(P_Apoptosis);
+        
+        %Apoptosis --> p53 to pRb via ECDK2
+        %p21cip
+        xd(P_p21cip) = c_Kpp1*(x(P_P53NucPhos)^4)/(c_Kpp2 + x(P_P53NucPhos)^4) - c_Kpp3 * x(P_p21cip);
+        %ECDK2
+        xd(P_ECDK2) = c_KpE1 - c_KpE2*x(P_p21cip)/(c_KpE3 + x(P_p21cip)) - c_KE * x(P_ECDK2);
+        %Cell Cycle Arrest, Note: kRb should either be on or off (represents gene)
+        %Note: Krb should have negative sign in front but will produce negative graphs, thus made positive
+        %Double check this later
+        xd(O_ARRESTSIGNAL) = (-KRb*c_Ka1*xd(P_ECDK2)*exp(-c_Ka1*(x(P_ECDK2)- c_Ka2)))/ ...
+            (1+ exp(-c_Ka1*(x(P_ECDK2)- c_Ka2)))^2;
+        %Cell Cycling, Note: Kg represents growth constant
+        xd(O_CELLCYCLING) = Kg*(1 - xd(O_ARRESTSIGNAL)); %Note - Increases indefinitely
+        
+        
+        
 
-		%Equations from Huzinker et al (https://www.ncbi.nlm.nih.gov/pubmed/20624280):
-		%Nuclear-p53 in negative feedback loop
-		xd(P_P53NucNeg) = sigma_p - (alpha_mdm2 * x(P_P53NucNeg)) - (k_f_pm * x(P_P53NucNeg) * x(M_MDM2NucNeg)) + (k_b_pm * x(P_P53_MDM2_Comp)) + (x(P_P53_MDM2_Comp) * gammma_mdm2_deg);
-		%Nuclear-Mdm2-mRNA in negative feedback loop. Note: I'm honestly not sure whether these should be instead measuring cytoplasmic concentration
-		xd(M_MDM2NucNeg) = (k_t_mdm2 * (x(P_P53NucNeg)^2)) - (beta_mdm2 * x(M_MDM2NucNeg));
-		%Nuclear-Mdm2 in negative feeback loop
-		xd(P_MDM2NucNeg) = (k_tl_mdm2 * x(M_MDM2NucNeg)) - (k_f_pm * x(P_P53NucNeg) * x(M_MDM2NucNeg)) + (x(M_MDM2NucNeg)) + (k_b_pm * x(P_P53_MDM2_Comp)) + (delta_mdm2 * x(P_P53_MDM2_Comp)) - (gammma_mdm2_deg * x(P_MDM2NucNeg));
-		%p53-MDM2 complex
-		xd(P_P53_MDM2_Comp) = (k_f_pm * x(P_P53NucNeg) * x(M_MDM2NucNeg)) - (k_b_pm * x(P_P53_MDM2_Comp)) - (delta_mdm2 * x(P_P53_MDM2_Comp)) - (gammma_mdm2_deg * x(P_P53_MDM2_Comp));
+
+
+		
