2bec7f952b74ee11162dbb8b0f44f999d74695d7
aspatti1257 <aspatti1257@gmail.com>
2017-08-27 14:29:06 -0400

    Linear Programming Model ITs to test and run SIM0 proof of concept. Includes sample Octave file, and ITs to get results from Octave and generate a multi-classifier RF model.

--- Testing/RandomForestTrainerIT.py
+++ Testing/RandomForestTrainerIT.py
@@ -10,6 +10,7 @@ from ThirdPartyProgramCaller import ThirdPartyProgramCaller
 
 class RandomForestTrainerIT(unittest.TestCase):
 
+
     log = logging.getLogger(__name__)
     log.setLevel(logging.INFO)
 
@@ -39,7 +40,7 @@ class RandomForestTrainerIT(unittest.TestCase):
     def getThirdPartyResult(self):
         return self.thirdPartyProgramCaller.callThirdPartyProgram(True)
 
-    def testRandomForestModelCreatedSuccessfully(self):
+    def testModelCreatedSuccessfully(self):
         genomes = self.initializeServicesAndCreateGenomes('WNT_ERK_crosstalk.octave', SupportedFileTypes.OCTAVE)
         third_party_result = self.getThirdPartyResult()
 
@@ -48,5 +49,30 @@ class RandomForestTrainerIT(unittest.TestCase):
 
         assert random_forest_model is not None
         assert random_forest_model.feature_importances_.size == len(genomes[1][0])  # num 'importances' == num features
+        assert len(random_forest_model.classes_) == 2
 
+    def testMultiClassifierModelCreatedSuccessfullyWithLinearProgramming(self):
+        attempts_at_multi_classification = 0
+        hit_more_than_two_outcomes = False
+        third_party_result = {}
+        genomes = []
 
+        while attempts_at_multi_classification < 10 and not hit_more_than_two_outcomes:
+            attempts_at_multi_classification += 1
+            self.log.info("Attempt number %s at getting multi-classifier results.", attempts_at_multi_classification)
+            genomes = self.initializeServicesAndCreateGenomes('linearProgrammingModel.octave', SupportedFileTypes.OCTAVE)
+            third_party_result = self.getThirdPartyResult()
+            classes = list(set(third_party_result.values()))
+            if len(classes) > 2:
+                hit_more_than_two_outcomes = True
+
+        if len(genomes) == 0 or third_party_result == {}:
+            self.log.error("Unable to return more than 2 classes from third party response.")
+            assert False
+
+        random_forest_trainer = RandomForestTrainer(genomes[1], third_party_result)
+        random_forest_model = random_forest_trainer.trainRandomForest(0.7)
+
+        assert random_forest_model is not None
+        assert random_forest_model.feature_importances_.size == len(genomes[1][0])  # num 'importances' == num features
+        assert len(random_forest_model.classes_) == 3
