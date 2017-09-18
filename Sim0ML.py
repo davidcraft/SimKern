122b4ed8cb7f575dec5460e7f9c1ed2ebea6c12e
Zhaoqi Wang <wangzhaoqi930829@hotmail.com>
2017-09-18 10:13:52 -0400

    support sim1

--- Sim0ML.py
+++ Sim0ML.py
@@ -6,23 +6,26 @@ import numpy as np
 
 def sim0test(feature_file,responses_file,num_permutations,training_per_cent):
     matrix = csvReader(feature_file)
-    num_examples = len(matrix)
-    num_genomes = len(matrix[0])
+    num_examples = matrix.shape[0]
+    num_genomes = matrix.shape[1]
+    print(matrix.shape)
     which = np.arange(num_genomes)
     responses = csvReader(responses_file)
+    assert (num_examples == len(responses))
     results = []
     for i in range(0, num_permutations):
         order = np.random.permutation(num_examples)
-        newmatrix = matrix[order[:, None], which]
+        newmatrix = matrix[order[:, None], order]
         newresponses = responses[order]
 
 
-        [trAcSVM, teAcSVM, totAcSVM] = runSvm(newresponses, newmatrix, 'rbf', training_per_cent)
+        [trAcSVM, teAcSVM, totAcSVM] = runSvm(newresponses, newmatrix, "precomputed", training_per_cent)
 
-        [trAcRF, teAcRF, totAcRF] = runRF(newresponses, newmatrix, training_per_cent)
-        results += [trAcSVM, teAcSVM, totAcSVM] + [trAcRF, teAcRF, totAcRF]
+        # [trAcRF, teAcRF, totAcRF] = runRF(newresponses, newmatrix, training_per_cent)
+        # results += [trAcSVM, teAcSVM, totAcSVM] + [trAcRF, teAcRF, totAcRF]
+        results += [trAcSVM, teAcSVM, totAcSVM]
 
-    results = np.array(results).reshape((num_permutations,6))
+    results = np.array(results).reshape((num_permutations,3))
     results = np.mean(results, axis=0)
     print(results, training_per_cent ,num_permutations )
 
@@ -31,4 +34,4 @@ def csvReader(file):
     return output
 
 
-sim0test('/Users/zhaoqiwang/Desktop/MGH/DSPP/GenomeFiles/Sim0GenomesMatrix.csv','/Users/zhaoqiwang/Desktop/MGH/DSPP/GenomeFiles/Sim0Output.csv',10,0.7)
+sim0test('/Users/zhaoqiwang/Desktop/MGH/DSPP/GenomeFiles/Sim1SimilarityMatrix.csv','/Users/zhaoqiwang/Desktop/MGH/DSPP/GenomeFiles/Sim0Output.csv',100,0.6)
